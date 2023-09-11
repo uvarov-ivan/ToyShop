2d46125912378e275c1f48808a8a3ca7e75a0865
Ivan Uvarov <uvarov_ivan@rambler.ru>
2023-09-11 22:09:40 +0300

    Finished first stage

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.ModelToy;
+import Model.Toy;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Toy bear = new Toy(1, "Bear", 1, 3);
+        Toy car = new Toy(2, "Car", 1, 7);
+        Toy car2 = new Toy(3, "Car2", 1, 5);
+        List<Toy> toys = new ArrayList<Toy>();
+        toys.add(bear);
+        toys.add(car);
+        toys.add(car2);
+        System.out.println(bear);
+        System.out.println(car);
+        System.out.println(car2);
+     
+        Toy prizeToy = ModelToy.raffle(toys);
+        System.out.println("\nПризовая игрушка:" + prizeToy);
+        for (Toy toy : toys) {
+            if (toy == prizeToy) {
+                toy.setInTotal(toy.getInTotal()-1);
+                if (toy.getInTotal() == 0) {
+                    toy.setFrequency(0);
+                }
+            }
+        }
+        
+        System.out.println(bear);
+        System.out.println(car);
+        System.out.println(car2);
     }
+
+
 }
